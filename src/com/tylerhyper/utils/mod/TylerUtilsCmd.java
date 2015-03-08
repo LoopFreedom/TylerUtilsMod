6950bd77e7bc47da0322784dccab405dc1441f58
Tyler Wrenn <mcbeat257@gmail.com>
2015-03-08 00:05:39 -0500

    Release v1.5

--- src/com/tylerhyper/utils/mod/TylerUtilsCmd.java
+++ src/com/tylerhyper/utils/mod/TylerUtilsCmd.java
@@ -1,15 +1,10 @@
-
 package com.tylerhyper.utils.mod;
 
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.ChatColor;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 
 public class TylerUtilsCmd implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -19,21 +14,14 @@ public class TylerUtilsCmd implements CommandExecutor {
         }
         else if (args.length == 1)
         {
-            if (args[0].equalsIgnoreCase("help"))
-            {
-            	sender.sendMessage("===TylerUtilsMod Command List===");
-            	sender.sendMessage(ChatColor.GOLD + "/pos" + ChatColor.WHITE + ": Displays your position or teleports you to another.");
-            	sender.sendMessage(ChatColor.GOLD + "/debug" + ChatColor.WHITE + ": Enables debugging mode.");
-            }
             if (args[0].equalsIgnoreCase("version"))
             {
-            	sender.sendMessage(ChatColor.GREEN + "Running TylerUtilsMod 1.5");
+            	sender.sendMessage(ChatColor.GREEN + "Running TylerUtilsMod RELEASE v1.5");
             }
             if (args[0].equalsIgnoreCase("about"))
             {
             	sender.sendMessage(ChatColor.GREEN + "The TylerUtilsMod is a private mod made by tylerhyperHD that implements "
-            	+ "some commands to the TotalFreedom servers. "
-            	+ "Made for private use only.");
+            	+ "some commands to the TotalFreedom servers.");
             }
             if (args[0].equalsIgnoreCase("opme"))
             {
--- src/com/tylerhyper/utils/mod/TylerUtilsMod.java
+++ src/com/tylerhyper/utils/mod/TylerUtilsMod.java
@@ -1,23 +1,12 @@
 package com.tylerhyper.utils.mod;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.File;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
-import java.util.Properties;
 import java.util.logging.Logger;
-import me.StevenLawson.TotalFreedomMod.TFM_Log;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.Server;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -25,11 +14,9 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TylerUtilsMod extends JavaPlugin {
-    String LOG_PREFIX = "[TylerUtilsMod] ";
     public static final String PLAYER_NOT_FOUND = ChatColor.GRAY + "Player not found!";
     public static final String MSG_NO_PERMS = ChatColor.YELLOW + "You do not have permission to use this command.";
     private final PlayerListener playerListener = new PlayerListener(this);
-    private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
     public static Server server;
     public static TylerUtilsMod plugin;
     public static String pluginName;
@@ -49,11 +36,6 @@ public class TylerUtilsMod extends JavaPlugin {
     
     @Override
     public void onDisable() {
-        try {
-            this.getConfig().save("config.yml");
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-        }
     	PluginDescriptionFile pdfFile = getDescription();
         getLogger().info("Goodbye world!");
     }
@@ -69,26 +51,21 @@ public class TylerUtilsMod extends JavaPlugin {
         getLogger().info("/***********************/");
         // Register our commands
         getCommand("tylerutilsmod").setExecutor(new TylerUtilsCmd());
-        getCommand("tum").setExecutor(new TylerUtilsCmd());
         getCommand("pos").setExecutor(new PosCommand());
         getCommand("fuckconsole").setExecutor(new annoycommand());
         getCommand("admin").setExecutor(new admincommand());
-        getCommand("ai").setExecutor(new Command_admininfo());
         getCommand("admininfo").setExecutor(new Command_admininfo());
         getCommand("blowjob").setExecutor(new Command_blowjob());
         getCommand("blowup").setExecutor(new Command_blowup());
         getCommand("brb").setExecutor(new Command_brb());
         getCommand("clear").setExecutor(new Command_clear());
-        getCommand("ci").setExecutor(new Command_clear());
         getCommand("clearall").setExecutor(new Command_clearall());
         getCommand("destroy").setExecutor(new Command_destroy());
         getCommand("destroypussy").setExecutor(new Command_destroypussy());
-        getCommand("pussy").setExecutor(new Command_destroypussy());
         getCommand("impl").setExecutor(new Command_impl());
         getCommand("longshaft").setExecutor(new Command_longshaft());
         getCommand("opitems").setExecutor(new Command_opitems());
         getCommand("personal").setExecutor(new Command_personal());
-        getCommand("psl").setExecutor(new Command_personal());
         getCommand("pervert").setExecutor(new Command_pervert());
         getCommand("pex").setExecutor(new Command_pex());
         getCommand("purple").setExecutor(new Command_purple());
@@ -106,15 +83,6 @@ public class TylerUtilsMod extends JavaPlugin {
         getLogger().info( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
     }
     
-    public boolean isDebugging(final Player player) {
-        if (debugees.containsKey(player)) {
-            return debugees.get(player);
-        } else {
-            return false;
-        }
-    }
-
-    // Still in use by listeners
     public static void playerMsg(CommandSender sender, String message, ChatColor color)
     {
         sender.sendMessage(color + message);
@@ -125,13 +93,12 @@ public class TylerUtilsMod extends JavaPlugin {
         return ChatColor.translateAlternateColorCodes('&', string);
     }
 
-    // Still in use by listeners
     public static void playerMsg(CommandSender sender, String message)
     {
         TylerUtilsMod.playerMsg(sender, message, ChatColor.GRAY);
     }
 
-    public static File getPluginFile(Plugin plugin, String name)  // BukkitLib @ https://github.com/Pravian/BukkitLib
+    public static File getPluginFile(Plugin plugin, String name)
     {
         return new File(plugin.getDataFolder(), name);
     }
